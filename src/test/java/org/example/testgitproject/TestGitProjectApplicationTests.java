79fa9e27b0c841fe1bd80113627ccbc88e71530f
Onisim Albarov <Onisim93@gmail.com>
2024-08-07 22:23:29 +0400

    init project

---
+++ src/test/java/org/example/testgitproject/TestGitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testgitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
